a0f6e4ac768cda7d4a2c161de428a4a24950c8ae
Christian Mendes <christianmendes645@gmail.com>
2025-08-19 21:50:59 -0300

    properties, Game e ORM

--- src/main/java/com/christianmendes/gameslist/entities/GameslistApplication.java
+++ src/main/java/com/christianmendes/gameslist/entities/GameslistApplication.java
@@ -1,4 +1,4 @@
-package com.christianmendes.gameslist;
+package com.christianmendes.gameslist.entities;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
