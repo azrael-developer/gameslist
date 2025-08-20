581e3b3c4338f679024f046c7a8cd1c8a8c706a0
Christian Mendes <christianmendes645@gmail.com>
2025-08-20 18:55:13 -0300

    GET Game By Id

--- src/main/java/com/christianmendes/gameslist/services/GameService.java
+++ src/main/java/com/christianmendes/gameslist/services/GameService.java
@@ -3,6 +3,9 @@ package com.christianmendes.gameslist.services;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.christianmendes.gameslist.dto.GameDTO;
 import com.christianmendes.gameslist.dto.GameMinDTO;
 import com.christianmendes.gameslist.entities.Games;
 import com.christianmendes.gameslist.repositories.GameRepository;
@@ -13,6 +16,13 @@ public class GameService {
     @Autowired  
     private GameRepository gameRepository;
 
+    @Transactional(readOnly = true)
+    public GameDTO findById(Long id) {
+        Games result = gameRepository.findById(id).get();
+        return new GameDTO(result);
+    }
+
+    @Transactional(readOnly = true)
     public List<GameMinDTO> findAll() {
             List<Games> result = gameRepository.findAll();
             List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
