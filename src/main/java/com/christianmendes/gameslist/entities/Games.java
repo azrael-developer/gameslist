2aa97df40ed5ccb9dcaaed6800138369617f8d3c
Christian Mendes <christianmendes645@gmail.com>
2025-08-19 22:09:37 -0300

    Game Seed

--- src/main/java/com/christianmendes/gameslist/entities/Games.java
+++ src/main/java/com/christianmendes/gameslist/entities/Games.java
@@ -22,7 +22,9 @@ public class Games {
     private String platforms;
     private Double score;
     private String imgUrl;
+    @Column(columnDefinition = "TEXT")
     private String shortDescription;
+    @Column(columnDefinition = "TEXT")
     private String longDescription;
 
     public Games() {
